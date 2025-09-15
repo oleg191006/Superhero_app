ae2bc524bd411836dde04861683aa645ff6b82f1
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-09-15 16:19:00 +0300

    feat: add Prisma setup and Superhero model

--- BACKEND/src/app.module.ts
+++ BACKEND/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { SuperheroModule } from './superhero/superhero.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    SuperheroModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

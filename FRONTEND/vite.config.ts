06c922fb7eba657416df9e71cc7d8e73c79e6d01
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-09-16 18:56:39 +0300

    feat: app path aliases for components,utils,services,types and hooks

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      types: path.resolve(__dirname, "src/types"),
+    },
+  },
+});

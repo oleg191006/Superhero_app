be27f7dc758fb9c832fe7bd3ce1bc69eb262d788
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-09-16 16:42:49 +0300

    feat: add getFullUrl helper

---
+++ FRONTEND/src/constants/request.ts
@@ -0,0 +1,14 @@
+export const URLs = {
+  superheroes: {
+    create: "/superheroes",
+    getAll: "/superheroes",
+    getById: "/superheroes/:id",
+    update: "/superheroes/:id",
+    delete: "/superheroes/:id",
+  },
+  images: {
+    upload: "/images",
+    getBySuperheroId: "/images/:superheroId",
+    delete: "/images/:id",
+  },
+};
